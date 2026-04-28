e65fff60e17c7477e0f8fc38a5db859ebb2c90c8
Stefani1113 <stefaniapuerta604@gmail.com>
2026-04-28 16:03:19 -0500

    feat : Implementación de migración y modelo

---
+++ app/Models/Tarea.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tarea extends Model
+{
+    protected $table = 'tarea';
+
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'prioridad',
+        'completada',
+        'fecha_limite'
+    ];
+
+    protected $casts = [
+        'completada' => 'boolean',
+        'fecha_limite' => 'date'
+    ];
+    
+}
